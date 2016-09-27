f894309fb3c759322eacc9a10cbe4a0fbb775a7b
Max Grønlund <max@synthmax.dk>
2016-09-27 15:21:19 +0200

    custom sql request to ommit views

--- lib/data_sampler/sample.rb
+++ lib/data_sampler/sample.rb
@@ -4,17 +4,22 @@ module DataSampler
 
   class Sample
 
-    def initialize(connection, rows_per_table = 1000)
-      @connection = connection
-      @rows_per_table = rows_per_table
+    #def initialize(connection, rows_per_table = 1000, database = 'test')
+    def initialize(options ={})
+      @connection = options[:conn]
+      @rows_per_table = options[:rows_per_table]
       @table_samples = {}
+      @schema = options[:schema]
       @computed = false
     end
 
     def compute!
-      @connection.tables.each do |table_name|
+
+      tables_without_views(@schema).each  do |table_name|
+        table_name = table_name.first
         # Workaround for inconsistent casing in table definitions (http://bugs.mysql.com/bug.php?id=60773)
         # table_name.downcase!
+        ##unless TABLE_TYPE == VIEW
         @table_samples[table_name] = TableSample.new(@connection, table_name, @rows_per_table)
       end
       warn "Sampling #{@table_samples.count} tables from database `#{@connection.current_database}`..."
@@ -43,6 +48,25 @@ module DataSampler
     def to_sql
       compute! unless @computed
       @table_samples.values.collect(&:to_sql) * "\n"
+    end
+
+    private
+
+    # fetch table_names from db without views
+    def tables_without_views(table_schema )
+      sql =  "SELECT TABLE_NAME  FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_TYPE = 'BASE TABLE' AND TABLE_SCHEMA = '#{table_schema}'"
+      execute_statement(sql)
+    end
+
+    # filter out error message
+    def execute_statement(sql)
+      results = @connection.execute(sql)
+      if results.present?
+        return results
+      else
+        warn "No tables found, please check schema name and DB content"
+        return nil
+      end
     end
 
   end
