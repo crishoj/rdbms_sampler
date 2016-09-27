a8df731026f1fbb3535901aca1f6c5f58e39b667
Max Grønlund <max@synthmax.dk>
2016-09-27 16:52:25 +0200

    sanitize sql string

--- lib/data_sampler/sample.rb
+++ lib/data_sampler/sample.rb
@@ -4,7 +4,6 @@ module DataSampler
 
   class Sample
 
-    #def initialize(connection, rows_per_table = 1000, database = 'test')
     def initialize(options ={})
       @connection = options[:conn]
       @rows_per_table = options[:rows_per_table] || 1000
@@ -19,7 +18,6 @@ module DataSampler
         table_name = table_name.first
         # Workaround for inconsistent casing in table definitions (http://bugs.mysql.com/bug.php?id=60773)
         # table_name.downcase!
-        ##unless TABLE_TYPE == VIEW
         @table_samples[table_name] = TableSample.new(@connection, table_name, @rows_per_table)
       end
       warn "Sampling #{@table_samples.count} tables from database `#{@connection.current_database}`..."
@@ -54,21 +52,9 @@ module DataSampler
 
     # fetch table_names from db without views
     def tables_without_views(table_schema )
-      sql =  "SELECT TABLE_NAME  FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_TYPE = 'BASE TABLE' AND TABLE_SCHEMA = '#{table_schema}'"
-      execute_statement(sql)
+      sql =  "SELECT TABLE_NAME  FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_TYPE = 'BASE TABLE' AND TABLE_SCHEMA = #{@connection.quote(table_schema)}"
+      @connection.execute(sql)
     end
-
-    # filter out error message
-    def execute_statement(sql)
-      results = @connection.execute(sql)
-      if results.present?
-        return results
-      else
-        warn "No tables found, please check schema name and DB content"
-        return nil
-      end
-    end
-
   end
 
 end
